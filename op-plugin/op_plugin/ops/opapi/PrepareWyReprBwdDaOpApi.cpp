b3ca48e4c9c2dff23cb25119416e5a9f52a8ccaf
woey <876362620@qq.com>
2026-03-06 16:28:14 +0800

    remove redundancy

--- op-plugin/op_plugin/ops/opapi/PrepareWyReprBwdDaOpApi.cpp
+++ op-plugin/op_plugin/ops/opapi/PrepareWyReprBwdDaOpApi.cpp
@@ -15,7 +15,6 @@
 #include "op_plugin/AclOpsInterface.h"
 #include "op_plugin/OpApiInterface.h"
 #include "op_plugin/utils/op_api_common.h"
-#include <iostream>  // 添加iostream头文件用于打印
 
 namespace op_api {
 using npu_preparation = at_npu::native::OpPreparation;
